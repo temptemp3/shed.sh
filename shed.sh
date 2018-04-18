0cb42495ba8b082b528c3551eb5d802f1f44b62c
Nicholas M Shellabarger <nicholas@h-maneki.net>
2018-04-19 00:02:39 +0900

    18 Apr 2018 EOD new requirements

--- shed.sh
+++ shed.sh
@@ -1,19 +1,44 @@
 #!/bin/bash
 ## shed
 ## - export workdspace slack history
-## version 0.0.2 - source slack functions
+## version 0.1.0 - new requirements
+## + date-oldest -> start-date
+## + filter-exclude-list override
 ## requires:
 ## - slack.sh
 ##################################################
 . $( dirname ${0} )/slack.sh/functions.sh
 ##################################################
+list-available-commands-filter-exclude-list() { 
+ cat << EOF
+$( ${FUNCNAME}-default )
+date-oldest
+EOF
+}
+#-------------------------------------------------
 slack-shed-env() {
  cat << EOF
 alias shed='bash $( pwd $( dirname ${0} ) )/shed.sh'
 EOF
 }
 #-------------------------------------------------
+slack-shed-start-date() { 
+ slack-shed-date-oldest ${@}
+}
+#-------------------------------------------------
+slack-shed-date-oldest-test() {
+ test "${date_oldest}"  &&
+ date --date="${date_oldest}" &&
+ true
+}
+#-------------------------------------------------
 slack-shed-date-oldest() { { local date_oldest ; date_oldest="${1}" ; }
+  {
+    ${FUNCNAME}-test || { 
+      slack-shed-help 
+      { error false ; false ; }
+    }
+  }
   {
     for-each-channel ${date_oldest}
   } 2>/dev/null
@@ -23,9 +48,10 @@ slack-shed-help() {
  cat << EOF
 slack-shed
 
-OPTIONS
+SUBCOMMANDS
 
-	date-oldest yyyy-mm-dd
+	start-date	yyyy-mm-dd
+	date-oldest	yyyy-mm-dd
 
 	- get channel histories between now and oldest date
 
